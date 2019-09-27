a1e44cbf50610e0c7d368c0132e02befb8b3d6fd
HOWISYUNI <blissoooooo@gmail.com>
2019-09-27 19:07:40 +0900

    add firebase user info

---
+++ SorrySori/app/src/main/java/appcontest/sorrysori/GpsTracker.java
@@ -0,0 +1,229 @@
+package appcontest.sorrysori;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+/**
+ * Ahmet Ertugrul OZCAN
+ * Cihazin konum bilgisini goruntuler
+ */
+public class GpsTracker extends Service implements LocationListener
+{
+    private final Context mContext;
+
+    // 기기에 gps가 있는지 확인
+    boolean isGPSEnabled = false;
+
+    // 장치의 데이터연결이 활성화됐는지 확인
+    boolean isNetworkEnabled = false;
+
+    boolean canGetLocation = false;
+
+    Location location;
+    // 위도
+    double latitude;
+    // 경도
+    double longitude;
+
+    // 위치 업데이트가 필요한 최소 변경량
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 미터
+
+    // 위치 업데이트하는데 필요한 최소 시간
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 분
+
+    // LocationManager nesnesi
+    protected LocationManager locationManager;
+
+    //CONSTRUCTOR
+    public GpsTracker(Context context)
+    {
+        this.mContext = context;
+        getLocation();
+    }
+
+    //
+    // Konum bilgisini dondurur
+    //
+    public Location getLocation()
+    {
+        try
+        {
+            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+
+            // GPS 사용 가능여부 확인
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // INTERNET 연결여부 확인
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled)
+            {
+
+            }
+            else
+            {
+                //퍼미션 체크를 위한 hasFineLocationPermission과 hasCoarseLocationPermission
+                int hasFineLocationPermission = ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION);
+                int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(mContext,Manifest.permission.ACCESS_COARSE_LOCATION);
+
+                //permission이 grant되지 않으면 null을 반환
+                if(hasFineLocationPermission == PackageManager.PERMISSION_GRANTED&&hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED){
+
+                }else{
+                    return null;
+                }
+
+                if (isNetworkEnabled)
+                {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("Network", "Network");
+                    if (locationManager != null)
+                    {
+                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        if (location != null)
+                        {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
+                }
+                // GPS'ten alinan konum bilgisi;
+                if (isGPSEnabled)
+                {
+                    if (location == null)
+                    {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null)
+                        {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null)
+                            {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Log.d("@@@",""+e.toString());
+        }
+
+        return location;
+    }
+
+    public double getLatitude()
+    {
+        if(location != null)
+        {
+            latitude = location.getLatitude();
+        }
+
+        return latitude;
+    }
+
+    public double getLongitude()
+    {
+        if(location != null)
+        {
+            longitude = location.getLongitude();
+        }
+
+        return longitude;
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+/*
+
+    public boolean canGetLocation()
+    {
+        return this.canGetLocation;
+    }
+
+    // Konum bilgisi kapali ise kullaniciya ayarlar sayfasina baglanti iceren bir mesaj goruntulenir
+    public void showSettingsAlert()
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Mesaj basligi
+        alertDialog.setTitle("GPS Kapalı");
+
+        // Mesaj
+        alertDialog.setMessage("Konum bilgisi alınamıyor. Ayarlara giderek gps'i aktif hale getiriniz.");
+
+        // Mesaj ikonu
+        //alertDialog.setIcon(R.drawable.delete);
+
+        // Ayarlar butonuna tiklandiginda
+        alertDialog.setPositiveButton("Ayarlar", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog,int which)
+            {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // Iptal butonuna tiklandiginda
+        alertDialog.setNegativeButton("İptal", new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+            }
+        });
+
+        // Mesaj kutusunu goster
+        alertDialog.show();
+    }
+*/
+
+    public void stopUsingGPS()
+    {
+        if(locationManager != null)
+        {
+            locationManager.removeUpdates(GpsTracker.this);
+        }
+    }
+}
